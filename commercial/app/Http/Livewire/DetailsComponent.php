436edf814fdac3a6b831457966c48ca49b86b33f
savaanakonda <marijaantovski@yahoo.com>
2023-06-25 15:08:45 +0200

    Index.php

--- commercial/app/Http/Livewire/DetailsComponent.php
+++ commercial/app/Http/Livewire/DetailsComponent.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Livewire;
+
 use App\Models\Product;
 use Livewire\Component;
+
 
 
 class DetailsComponent extends Component
@@ -16,6 +18,8 @@ class DetailsComponent extends Component
     public function render()
     {
         $product = Product::where('slug',$this->slug)->first();
+        $rproducts = Product::where('category_id', $product->category_id)->inRandomOrder()->limit(4)->get();
+        $nproducts = Product::latest()->take(4)->get();
         return view('livewire.details-component', ['product' => $product]);
     }
 }
