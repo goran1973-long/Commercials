bd101b8faf94733cc25c49ec6c889f3737467197
savaanakonda <marijaantovski@yahoo.com>
2023-07-22 07:21:35 +0200

    Filter

--- commercial/app/Http/Livewire/SearchComponent.php
+++ commercial/app/Http/Livewire/SearchComponent.php
@@ -2,12 +2,64 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use App\Models\Category;
+use Livewire\WithPagination;
+use Cart;
 
 class SearchComponent extends Component
+
 {
+    use WithPagination;
+    public $pageSize = 12;
+    public $orderBy = 'Default Sorting';
+
+    public $q;
+
+    public $search_term;
+
+    public function mount()
+    {
+        $this->fill(request()->only('q'));
+        $this->search_term = '%' . $this->q . '%';
+    }
+
+    public function store($product_id,$product_name,$product_price)
+    {
+      Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
+      session()->flash('success_message', 'Item added in the Cart');
+      return redirect()->route('shop.cart');
+    }
+
+    public function changePageSize($size)
+    {
+      $this->pageSize = $size; 
+    }
+
+    public function changeOrderBy($order)
+    {
+      $this->orderBy = $order;
+    }
     public function render()
     {
-        return view('livewire.search-component');
+      if ($this->orderBy == 'Price:Low to high') {
+        $products = Product::where('name','like', $this->search_term)->orderBy('regular_price','ASC')->paginate($this->pageSize);
+      }
+
+      elseif ($this->orderBy == 'Price:High to low') {
+        $products = Product::where('name','like', $this->search_term)->orderBy('regular_price','DESC')->paginate($this->pageSize);
+      }
+
+      elseif ($this->orderBy == 'Sort by Newness') {
+        $products = Product::where('name','like', $this->search_term)->orderBy('created_at','DESC')->paginate($this->pageSize);
+      }
+
+      else {
+        $products = Product::where('name','like', $this->search_term)->paginate($this->pageSize);
+      }
+        $categories = Category::orderBy('name', 'ASC'); 
+        return view('livewire.search-component', ['products' => $products, 'categories' => $categories]);
     }
 }
+
--- commercial/app/Http/Livewire/ShopComponent.php
+++ commercial/app/Http/Livewire/ShopComponent.php
@@ -15,6 +15,9 @@ class ShopComponent extends Component
     public $pageSize = 12;
     public $orderBy = 'Default Sorting';
 
+    public $min_value = 0;
+    public $max_value = 1000;
+
     public function store($product_id,$product_name,$product_price)
     {
       Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
@@ -34,19 +37,19 @@ class ShopComponent extends Component
     public function render()
     {
       if ($this->orderBy == 'Price:Low to high') {
-        $products = Product::orderBy('regular_price','ASC')->paginate($this->pageSize);
+        $products = Product::whereBetween('regular_price', [$this->min_value, $this->max_value])->orderBy('regular_price','ASC')->paginate($this->pageSize);
       }
 
       elseif ($this->orderBy == 'Price:High to low') {
-        $products = Product::orderBy('regular_price','DESC')->paginate($this->pageSize);
+        $products = Product::whereBetween('regular_price', [$this->min_value, $this->max_value])->orderBy('regular_price','DESC')->paginate($this->pageSize);
       }
 
       elseif ($this->orderBy == 'Sort by Newness') {
-        $products = Product::orderBy('created_at','DESC')->paginate($this->pageSize);
+        $products = Product::whereBetween('regular_price', [$this->min_value, $this->max_value])->orderBy('created_at','DESC')->paginate($this->pageSize);
       }
 
       else {
-        $products = Product::paginate($this->pageSize);
+        $products = Product::whereBetween('regular_price', [$this->min_value, $this->max_value])->paginate($this->pageSize);
       }
         $categories = Category::orderBy('name', 'ASC'); 
         return view('livewire.shop-component', ['products' => $products, 'categories' => $categories]);
